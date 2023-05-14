5a026b3d2be860bdb0cbba14ab5a7b510a7e3330
jotaherrera <juanherreramen13@gmail.com>
2023-05-14 16:42:29 -0500

    Update User screen to screen center

--- BillingSystem/UsersScreen.Designer.cs
+++ BillingSystem/UsersScreen.Designer.cs
@@ -29,91 +29,88 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Users));
-            this.dgvAdmin = new System.Windows.Forms.DataGridView();
-            this.clmIdUs = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clmUser = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.clmPassword = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.BackUsersButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvAdmin)).BeginInit();
-            this.SuspendLayout();
+            dgvAdmin = new DataGridView();
+            clmIdUs = new DataGridViewTextBoxColumn();
+            clmUser = new DataGridViewTextBoxColumn();
+            clmPassword = new DataGridViewTextBoxColumn();
+            BackUsersButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvAdmin).BeginInit();
+            SuspendLayout();
             // 
             // dgvAdmin
             // 
-            this.dgvAdmin.AllowUserToAddRows = false;
-            this.dgvAdmin.AllowUserToDeleteRows = false;
-            this.dgvAdmin.AllowUserToResizeColumns = false;
-            this.dgvAdmin.AllowUserToResizeRows = false;
-            this.dgvAdmin.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(249)))), ((int)(((byte)(244)))));
-            this.dgvAdmin.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dgvAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dgvAdmin.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.clmIdUs,
-            this.clmUser,
-            this.clmPassword});
-            this.dgvAdmin.Location = new System.Drawing.Point(16, 13);
-            this.dgvAdmin.Margin = new System.Windows.Forms.Padding(0);
-            this.dgvAdmin.Name = "dgvAdmin";
-            this.dgvAdmin.ReadOnly = true;
-            this.dgvAdmin.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.dgvAdmin.RowTemplate.Height = 25;
-            this.dgvAdmin.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.dgvAdmin.Size = new System.Drawing.Size(543, 320);
-            this.dgvAdmin.TabIndex = 5;
-            this.dgvAdmin.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAdmin_CellContentClick);
+            dgvAdmin.AllowUserToAddRows = false;
+            dgvAdmin.AllowUserToDeleteRows = false;
+            dgvAdmin.AllowUserToResizeColumns = false;
+            dgvAdmin.AllowUserToResizeRows = false;
+            dgvAdmin.BackgroundColor = Color.FromArgb(255, 249, 244);
+            dgvAdmin.BorderStyle = BorderStyle.None;
+            dgvAdmin.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dgvAdmin.Columns.AddRange(new DataGridViewColumn[] { clmIdUs, clmUser, clmPassword });
+            dgvAdmin.Location = new Point(16, 13);
+            dgvAdmin.Margin = new Padding(0);
+            dgvAdmin.Name = "dgvAdmin";
+            dgvAdmin.ReadOnly = true;
+            dgvAdmin.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            dgvAdmin.RowTemplate.Height = 25;
+            dgvAdmin.ScrollBars = ScrollBars.None;
+            dgvAdmin.Size = new Size(543, 320);
+            dgvAdmin.TabIndex = 5;
+            dgvAdmin.CellContentClick += dgvAdmin_CellContentClick;
             // 
             // clmIdUs
             // 
-            this.clmIdUs.HeaderText = "ID";
-            this.clmIdUs.Name = "clmIdUs";
-            this.clmIdUs.ReadOnly = true;
+            clmIdUs.HeaderText = "ID";
+            clmIdUs.Name = "clmIdUs";
+            clmIdUs.ReadOnly = true;
             // 
             // clmUser
             // 
-            this.clmUser.HeaderText = "User";
-            this.clmUser.Name = "clmUser";
-            this.clmUser.ReadOnly = true;
-            this.clmUser.Width = 200;
+            clmUser.HeaderText = "User";
+            clmUser.Name = "clmUser";
+            clmUser.ReadOnly = true;
+            clmUser.Width = 200;
             // 
             // clmPassword
             // 
-            this.clmPassword.HeaderText = "Password";
-            this.clmPassword.Name = "clmPassword";
-            this.clmPassword.ReadOnly = true;
-            this.clmPassword.Width = 200;
+            clmPassword.HeaderText = "Password";
+            clmPassword.Name = "clmPassword";
+            clmPassword.ReadOnly = true;
+            clmPassword.Width = 200;
             // 
             // BackUsersButton
             // 
-            this.BackUsersButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(142)))), ((int)(((byte)(112)))));
-            this.BackUsersButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.BackUsersButton.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BackUsersButton.ForeColor = System.Drawing.Color.White;
-            this.BackUsersButton.Location = new System.Drawing.Point(156, 346);
-            this.BackUsersButton.Name = "BackUsersButton";
-            this.BackUsersButton.Size = new System.Drawing.Size(263, 27);
-            this.BackUsersButton.TabIndex = 7;
-            this.BackUsersButton.Text = "Back";
-            this.BackUsersButton.UseVisualStyleBackColor = false;
-            this.BackUsersButton.Click += new System.EventHandler(this.BackUsersButton_Click);
+            BackUsersButton.BackColor = Color.FromArgb(12, 142, 112);
+            BackUsersButton.FlatStyle = FlatStyle.Popup;
+            BackUsersButton.Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            BackUsersButton.ForeColor = Color.White;
+            BackUsersButton.Location = new Point(156, 346);
+            BackUsersButton.Name = "BackUsersButton";
+            BackUsersButton.Size = new Size(263, 27);
+            BackUsersButton.TabIndex = 7;
+            BackUsersButton.Text = "Back";
+            BackUsersButton.UseVisualStyleBackColor = false;
+            BackUsersButton.Click += BackUsersButton_Click;
             // 
             // Users
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(249)))), ((int)(((byte)(244)))));
-            this.ClientSize = new System.Drawing.Size(574, 385);
-            this.ControlBox = false;
-            this.Controls.Add(this.BackUsersButton);
-            this.Controls.Add(this.dgvAdmin);
-            this.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(590, 424);
-            this.MinimumSize = new System.Drawing.Size(590, 424);
-            this.Name = "Users";
-            this.Text = "Users";
-            this.Load += new System.EventHandler(this.Users_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvAdmin)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 249, 244);
+            ClientSize = new Size(574, 385);
+            ControlBox = false;
+            Controls.Add(BackUsersButton);
+            Controls.Add(dgvAdmin);
+            Font = new Font("Microsoft Sans Serif", 8.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximumSize = new Size(590, 424);
+            MinimumSize = new Size(590, 424);
+            Name = "Users";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Users";
+            Load += Users_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAdmin).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
